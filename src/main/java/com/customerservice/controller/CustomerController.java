14006ecc7cc37034cb140001d83e9be943a45c6f
MSI\Minh Tan <tanlun04112000@gmail.com>
2021-11-26 09:28:41 +0700

    ratelimiter finish

--- src/main/java/com/customerservice/controller/CustomerController.java
+++ src/main/java/com/customerservice/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.customerservice.entity.Customer;
 import com.customerservice.service.CustomerService;
 import com.customerservice.vo.ResponseTemplateVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,7 +27,7 @@ public class CustomerController
     }
 
     @GetMapping("/{id}")
-    public ResponseTemplateVO getCustomerWithFood(@PathVariable("id") Long id){
+    public ResponseEntity<ResponseTemplateVO> getCustomerWithFood(@PathVariable("id") Long id){
         return customerService.getCustomerWithFood(id);
     }
 }
--- src/main/java/com/customerservice/service/CustomerService.java
+++ src/main/java/com/customerservice/service/CustomerService.java
@@ -1,10 +1,14 @@
 package com.customerservice.service;
 
 import com.customerservice.entity.Customer;
+import com.customerservice.entity.ExceptionHandling;
 import com.customerservice.entity.Food;
 import com.customerservice.repository.CustomerRepository;
 import com.customerservice.vo.ResponseTemplateVO;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,7 +31,8 @@ public class CustomerService {
         return customerRepository.findAll();
     }
 
-    public ResponseTemplateVO getCustomerWithFood(Long id) {
+    @RateLimiter(name = "multiRate", fallbackMethod = "fallBackMethod")
+    public ResponseEntity<ResponseTemplateVO> getCustomerWithFood(Long id) {
         ResponseTemplateVO vo = new ResponseTemplateVO();
         Customer customer = customerRepository.findById(id).get();
         vo.setCustomer(customer);
@@ -36,6 +41,11 @@ public class CustomerService {
                         Food.class);
 
         vo.setFood(food);
-        return vo;
+        return new ResponseEntity<ResponseTemplateVO>(vo, HttpStatus.OK);
+    }
+
+    private ResponseEntity<ExceptionHandling> fallBackMethod(RuntimeException exception){
+        ExceptionHandling handling = new ExceptionHandling("Quá nhiều yêu cầu xin hãy reload lại vài giây sau");
+        return new ResponseEntity<ExceptionHandling>(handling, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
